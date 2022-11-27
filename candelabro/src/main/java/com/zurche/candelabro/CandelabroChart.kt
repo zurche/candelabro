cc962de49118b88df85542015c48559e4169465a
Alejandro Zurcher <alejandro.zurcher@gmail.com>
2022-11-27 10:26:36 +0000

    Adding basic compose support for project.

--- candelabro/src/main/java/com/zurche/candelabro/CandelabroChart.kt
+++ candelabro/src/main/java/com/zurche/candelabro/CandelabroChart.kt
@@ -1,4 +1,12 @@
 package com.zurche.candelabro
 
-class CandelabroChart {
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.zurche.candelabro.model.StockDayValue
+
+@Preview("Main View")
+@Composable
+fun CandelabroChart(dailyValues: List<StockDayValue>? = null) {
+    Text("Hello")
 }
---
+++ candelabro/src/main/java/com/zurche/candelabro/model/StockDayValue.kt
@@ -0,0 +1,11 @@
+package com.zurche.candelabro.model
+
+import java.util.Date
+
+data class StockDayValue(
+    val date: Date,
+    val openValue: Double,
+    val closeValue: Double,
+    val maxValue: Double,
+    val minValue: Double
+)
